4aec27d7c91f9b8f8950d415a07e2a2620052c78
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-08-26 03:08:46 +0530

    8.11

---
+++ part08-Part08_11.SameDate/src/main/java/SimpleDate.java
@@ -0,0 +1,75 @@
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(SimpleDate other) {
+        if (this.year < other.year) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month < other.month) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month == other.month
+                && this.day < other.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int differenceInYears(SimpleDate other) {
+        if (earlier(other)) {
+            return other.differenceInYears(this);
+        }
+
+        int yearRemoved = 0;
+
+        if (this.month < other.month) {
+            yearRemoved = 1;
+        } else if (this.month == other.month && this.day < other.day) {
+            yearRemoved = 1;
+        }
+
+        return this.year - other.year - yearRemoved;
+    }
+
+    public boolean equals(SimpleDate object) {
+        // compare address
+        if (this == object) {
+            return true;
+        }
+
+        // compare type
+        if (!(object instanceof SimpleDate)) {
+            return false;
+        }
+
+        // typecast object
+        SimpleDate comparedObject = (SimpleDate) object;
+
+        if (comparedObject.day == this.day && comparedObject.month == this.month && comparedObject.year == this.year) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
