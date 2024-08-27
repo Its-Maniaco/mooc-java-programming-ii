245a4594054b8803f2390a1177cb1e5e4b92703a
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-08-27 19:17:49 +0530

    10.14

---
+++ part10-Part10_14.Literature/src/main/java/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    private String title;
+    private int age;
+
+    public Book(String title, int age) {
+        this.title = title;
+        this.age = age;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+
+        return this.title + " (recommended for " + this.age + " year-olds or older)";
+    }
+}
